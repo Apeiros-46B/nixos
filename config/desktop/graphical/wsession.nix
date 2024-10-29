508e667790db32d1f6f2076dc49ce785cbdd15fa
Apeiros-46B <Apeiros-46B@users.noreply.github.com>
2024-10-29 15:04:44 -0700

    no more wayland

--- config/desktop/graphical/wsession.nix
+++
@@ -1,8 +0,0 @@
-{ functions, ... }:
-
-functions.linkImpure "river" {
-	programs.river.enable = true;
-	hm.xdg.configFile."river/scale".text = ''
-		scale=1
-	'';
-}
--- config/desktop/graphical/xserver.nix
+++ config/desktop/graphical/xserver.nix
@@ -27,4 +27,15 @@
 	hm.xsession.profileExtra = ''
 		${pkgs.xorg.xset}/bin/xset r rate 350 75
 	'';
+
+	hm.home.pointerCursor = {
+		name = "phinger-cursors-light";
+		package = pkgs.phinger-cursors;
+		size = 32;
+		gtk.enable = true;
+		x11 = {
+			enable = true;
+			defaultCursor = "left_ptr";
+		};
+	};
 }
