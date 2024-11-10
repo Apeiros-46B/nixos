bcfed32bd82cb05308aa8911697d4ec3026d6435
Apeiros-46B <Apeiros-46B@users.noreply.github.com>
2024-11-09 22:00:55 -0800

    EMERGENCY FIX

--- config/common/services/ssh.nix
+++ config/common/services/ssh.nix
@@ -19,27 +19,27 @@
 		group = "root";
 		mode = "0400";
 	};
-	security.pam.services.sshd.text =
-		let
-			hook = (pkgs.writeShellScriptBin "ssh-discord" ''
-				WEBHOOK_URL="$(cat '${config.sops.secrets.ssh-discord-webhook-url.path}')"
-				MENTION='<@${globals.discordUid}>'
+	# security.pam.services.sshd.text =
+	# 	let
+	# 		hook = (pkgs.writeShellScriptBin "ssh-discord" ''
+	# 			WEBHOOK_URL="$(cat '${config.sops.secrets.ssh-discord-webhook-url.path}')"
+	# 			MENTION='<@${globals.discordUid}>'
 
-				case "$PAM_TYPE" in
-					open_session)
-						PAYLOAD=" { \"content\": \"⚠️ $MENTION user \`$PAM_USER\` logged in to \`$HOSTNAME\` from $PAM_RHOST\" }"
-						;;
-					close_session)
-						PAYLOAD=" { \"content\": \"⚠️ $MENTION user \`$PAM_USER\` logged out of \`$HOSTNAME\` from $PAM_RHOST\" }"
-						;;
-				esac
+	# 			case "$PAM_TYPE" in
+	# 				open_session)
+	# 					PAYLOAD=" { \"content\": \"⚠️ $MENTION user \`$PAM_USER\` logged in to \`$HOSTNAME\` from $PAM_RHOST\" }"
+	# 					;;
+	# 				close_session)
+	# 					PAYLOAD=" { \"content\": \"⚠️ $MENTION user \`$PAM_USER\` logged out of \`$HOSTNAME\` from $PAM_RHOST\" }"
+	# 					;;
+	# 			esac
 
-				if [ -n "$PAYLOAD" ] ; then
-					curl -X POST -H 'Content-Type: application/json' -d "$PAYLOAD" "$WEBHOOK_URL"
-				fi
-			'');
-		in
-	''
-		session	optional	pam_exec.so	${hook}
-	'';
+	# 			if [ -n "$PAYLOAD" ] ; then
+	# 				curl -X POST -H 'Content-Type: application/json' -d "$PAYLOAD" "$WEBHOOK_URL"
+	# 			fi
+	# 		'');
+	# 	in
+	# ''
+	# 	session	optional	pam_exec.so	${hook}
+	# '';
 }
