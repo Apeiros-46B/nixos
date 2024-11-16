ff87bbc1424d25819411fc6348c4df2a1735e5ed
Apeiros-46B <Apeiros-46B@users.noreply.github.com>
2024-11-15 16:32:17 -0800

    acme

--- config/server/services/acme.nix
+++ config/server/services/acme.nix
@@ -1,7 +1,19 @@
-{ ... }:
+{ config, ... }:
 
 {
-	security.acme = {
+	sops.secrets.cloudflare-acme-tokens = {
+		sopsFile = ./Secrets.yaml;
+		owner = "root";
+		group = "root";
+		mode = "0400";
+	};
 
+	security.acme = {
+		acceptTerms = true;
+		defaults.email = "apeiros46@gmail.com";
+		certs."*.apeiros.xyz" = {
+			dnsProvider = "cloudflare";
+			environmentFile = config.sops.secrets.cloudflare-acme-tokens.path;
+		};
 	};
 }
