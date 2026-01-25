1e0a528f8fa073b8ee90fccb961fc2aa495c21ad
Apeiros-46B <Apeiros-46B@users.noreply.github.com>
2026-01-25 09:17:21 -0800

    copyparty stuff

--- hosts/bastion/services/storage/copyparty.nix
+++ hosts/bastion/services/storage/copyparty.nix
@@ -3,6 +3,8 @@
 let
 	port = 4000;
 	domain = "box.apeiros.xyz";
+	lanRange = "10.0.0.0/8";
+	tailscaleRange = "100.64.0.0/10";
 in {
 	imports = [
 		inputs.copyparty.nixosModules.default
@@ -15,7 +17,13 @@ in {
 		};
 		groups.nas.members = [ "root" "copyparty" ];
 	};
-	environment.systemPackages = [ pkgs.copyparty ];
+	systemd.services.copyparty.path = with pkgs; [
+		python313Packages.paramiko # TODO: sftp, ftps, ftp, webdav? which one?
+		python313Packages.mutagen
+		python313Packages.pyvips
+		python313Packages.rawpy
+		ffmpeg-headless
+	];
 
 	networking.firewall.allowedTCPPorts = [ port ];
 	services.cloudflared.tunnels."72d0b7dc-fc9b-460e-9d70-c873c5e97fb8".ingress = {
@@ -28,7 +36,7 @@ in {
 		group = "nas";
 		mode = "0400";
 	};
-	sops.secrets.copyparty-apeiros-password = {
+	sops.secrets.copyparty-admin-password = {
 		sopsFile = ./Secrets.yaml;
 		owner = "copyparty";
 		group = "nas";
@@ -40,53 +48,76 @@ in {
 		user = "copyparty";
 		group = "nas";
 		settings = {
+			no-reload = true;
+
+			# connection
 			i = "0.0.0.0";
 			p = [ port ];
-			rproxy = -1;
-			no-reload = true;
+			rproxy = -1; # for cf tunnel
+			xff-hdr = "cf-proxying-ip";
+
+			# security
+			no-readme = true;
+			no-logues = true; # TODO: figure out how to enable this only for inbox volume
+			no-robots = true;
 			usernames = true;
+			ipr = "${lanRange},${tailscaleRange}=admin";
+			nih = true;
+			xdev = true;
 			xvol = true;
+
+			# fs
+			# no-clone = true; # TODO: figure out if we need this for syncthing compat?
+			re-maxage = 60; # syncthing compat
+			no-mtag-ff = true;
+
+			# LAN access
+			z = true;
+			z-on = [ lanRange tailscaleRange ];
+			# TODO: set it up over nginx so i can access `bastion.local` directly on 80/443
+			# TODO: hooks to notify of uploads/downloads of large files over discord webhook
+
+			# appearance
+			og-ua = "(Discord|Twitter|Slack)bot";
+			og-site = domain;
+			theme = 3;
 		};
 		accounts = {
 			inbox.passwordFile = "${config.sops.secrets.copyparty-inbox-password.path}";
-			apeiros.passwordFile = "${config.sops.secrets.copyparty-apeiros-password.path}";
+			admin.passwordFile = "${config.sops.secrets.copyparty-admin-password.path}";
 		};
 		volumes = {
 			"/inbox" = {
 				path = "/nas/inbox";
 				access = {
 					wG = [ "inbox" ];
-					A = [ "apeiros" ];
+					A = [ "admin" ];
 				};
 				flags = {
 					fk = 16;
 					d2t = true;
-					magic = true;
-					norobots = true;
+					dthumb = true;
+					nohtml = true;
 				};
 			};
 			"/public" = {
 				path = "/nas/public";
 				access = {
 					r = "*";
-					A = [ "apeiros" ];
+					A = [ "admin" ];
 				};
 				flags = {
-					magic = true;
 					og = true;
-					og_site = domain;
 				};
 			};
 			"/private" = {
 				path = "/nas/private";
 				access = {
-					A = [ "apeiros" ];
+					A = [ "admin" ];
 				};
 				flags = {
 					e2dsa = true;
 					e2ts = true;
-					magic = true;
-					norobots = true;
 				};
 			};
 		};
